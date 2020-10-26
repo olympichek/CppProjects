54b59ec6ca41b22a754c0730cc771fe2e782151c
olympichek <olympichek1@gmail.com>
2020-10-26 18:46:46 +0000

    task50b3 added

---
+++ Tasks12/task50b3.cpp
@@ -0,0 +1,23 @@
+#include <cstdio>
+
+int main() {
+    int k;
+    printf("Enter k: ");
+    scanf("%d", &k);
+    int n = 1, t = 9, r = 9, k1, x, i, res;
+    while(k > r) {
+        n++;
+        t *= 10;
+        r += n*t;
+    }
+    k1 = k - r + n*t;
+    x = (t/9) + (k1 - 1)/n;
+    i = n - ( (k1 - 1)%n );
+    while(i > 0) {
+        res = x % 10;
+        x /= 10;
+        i--;
+    }
+    printf("res: %d\n", res);
+    return 0;
+}
