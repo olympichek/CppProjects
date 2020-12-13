1d2f0b5312f9d4f65023ed8319330e09efdb9db9
olympichek <olympichek1@gmail.com>
2020-12-13 02:48:14 +0200

    added list template

---
+++ List/cmake-build-debug/CMakeFiles/Task01.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Task01.dir/main.cpp.o"
+  "Task01"
+  "Task01.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Task01.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
